0f05d160fb1b34a19acfe50b73bf5dccd2b0c1d0
Sanjay Pandey <vigyanhoon@yahoo.co.in>
2020-10-17 17:34:21 +0530

    add share option

--- views/ImageView.tsx
+++ views/ImageView.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
-  Image,
-  Button,
-  Text, ImageBackground
+  Text,
+  ImageBackground,
+  TouchableOpacity
 } from 'react-native'
 import { useDispatch } from 'react-redux';
 import { removeImage } from '../reducers/imageSlice';
 
+import Icon from 'react-native-vector-icons/Feather';
+import Share from "react-native-share";
+
 const ImageView = ({ navigation, route: { params: { image } } }) => {
   const dispatch = useDispatch()
+  const [showMenu, setShowMenu] = useState(false)
 
   const deletePressed = () => {
     (async () => {
@@ -19,20 +23,44 @@ const ImageView = ({ navigation, route: { params: { image } } }) => {
     })()
   }
 
+  const sharePressed = () => {
+    const options = {
+      title: 'Share title',
+      message: image.name,
+      url: image.path
+    }
+    console.log(options)
+    Share.open(options)
+      .then((res) => { setShowMenu(false) })
+      .catch((err) => { err && console.log(err); });
+  }
+
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <View style={styles.deleteButton}>
-          <Button color={'red'} onPress={deletePressed} title="Delete" />
-        </View>
+        <TouchableOpacity style={styles.moreButton} onPress={() => setShowMenu(isOpen => !isOpen)}>
+          <Icon name="more-vertical" size={30} color="#000" />
+        </TouchableOpacity>
       ),
     });
   }, [navigation]);
 
+  const Menu = () => {
+    return <View style={styles.menuContainer}>
+      <TouchableOpacity onPress={deletePressed}>
+        <Text style={styles.menuItem}>Delete</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={sharePressed}>
+        <Text style={styles.menuItem}>Share</Text>
+      </TouchableOpacity>
+    </View>
+  }
+
   return (
     <>
-      <View style={styles.body}>
+      <View style={styles.body} onTouchEnd={()=>setShowMenu(false)}>
         <ImageBackground style={styles.image} source={{ uri: image.path }}>
+          {showMenu && <Menu />}
           <Text style={styles.imageLabel}>{image.name}</Text>
         </ImageBackground>
       </View>
@@ -43,14 +71,31 @@ const ImageView = ({ navigation, route: { params: { image } } }) => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
+    justifyContent: 'flex-end'
   },
   image: {
     flex: 1,
-    justifyContent:'flex-end',
-    alignItems:'center',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
-  deleteButton: {
-    marginRight: 20
+  moreButton: {
+    height: 50,
+    width: 50,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  menuContainer: {
+    backgroundColor: 'white',
+    position: "absolute",
+    alignSelf: "flex-end",
+    top: 0,
+  },
+  menuItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    fontSize: 25,
+    borderColor: 'black',
+    borderWidth: 1
   },
   imageLabel: {
     color: 'white',
